2e9323ebb6c668085b7a8a3a4f4820c29984a38d
jojopirker <j.pirker@gmail.com>
2023-01-20 07:52:01 +0100

    some minor touch ups to about page and its components

--- website/src/pages/about.tsx
+++ website/src/pages/about.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { CallToAction } from "src/components/CallToAction";
 import { Container } from "src/components/Container";
-import Roadmap from "src/components/Roadmap";
-import Services from "src/components/Services";
-import Vision from "src/components/Vision";
+import { getTransparentHeaderLayout } from "src/components/Layout";
+import Roadmap from "src/components/About/Roadmap";
+import Services from "src/components/About/Services";
+import Vision from "src/components/About/Vision";
 
 const AboutPage = () => {
   return (
@@ -11,17 +12,17 @@ const AboutPage = () => {
       <Container className="">
         <div className="grid gap-16 items-center py-20 md:py-40 lg:grid-cols-2">
           <div className="m-auto order-2 lg:order-1">
-            <Image src="/images/logos/logo.png" width={450} height={450} alt="temp-image" />
+            <Image src="/images/logos/logo.svg" width={450} height={450} alt="temp-image" />
           </div>
           <div className="space-y-8 order-1 lg:order-2">
             <div>
-              <h1 className="text-4xl mb-6">What is OpenAssistant?</h1>
-              <p className="text-2xl">
+              <h1 className="text-3xl font-medium tracking-tight sm:text-4xl">What is OpenAssistant?</h1>
+              <p className="mt-4 text-lg">
                 OpenAssistant is a chat-based assistant that understands tasks, can interact with third-party systems,
                 and retrieve information dynamically to do so.
               </p>
             </div>
-            <p className="text-2xl">
+            <p className="mt-4 text-lg">
               It can be extended and personalized easily and is developed as free, open-source software.
             </p>
           </div>
@@ -35,5 +36,7 @@ const AboutPage = () => {
     </div>
   );
 };
+
+AboutPage.getLayout = getTransparentHeaderLayout;
 
 export default AboutPage;
